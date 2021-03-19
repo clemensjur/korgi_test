5abe6667560c0273c6931ed8658440655df79a6e
Wrager02 <alre0209@gmail.com>
2021-03-19 15:38:12 +0100

    Add improved blur

--- resources/js/Layouts/PageLayout.vue
+++ resources/js/Layouts/PageLayout.vue
@@ -4,6 +4,7 @@
         <navigation :bus="bus" :user="$page.props.user.name"/>
         <navbar :bus="bus"/>
         <main @click.prevent="isActive ? bus.$emit('toggleMenu') : null" :class="{'blurred': isActive}">
+            <div class="blur"></div>
             <slot></slot>
         </main>
     </div>
@@ -43,9 +44,21 @@ export default {
     overflow: hidden;
 }
 
-main.blurred {
-    filter: blur(2px) brightness(70%) opacity(100);
-    overflow: hidden;
+.blur {
+    top: 0;
+    position: fixed;
+
+    z-index: 1;
+    width: 100vw;
+    height: 100vh;
+    backdrop-filter: blur(2px) brightness(70%) opacity(100);
+    opacity: 0;
+    pointer-events: none;
+}
+
+main.blurred .blur {
+    opacity: 1;
+    pointer-events: all;
 }
 
 main {
